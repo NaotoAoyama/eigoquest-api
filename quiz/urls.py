2f817142d118b1cd64c994a2d3e4272019e42894
Naoto Aoyama <naoto.aoyama051217@gmail.com>
2025-10-21 15:35:46 +0900

    "Create QuizView (CBV) to display 10 random questions"

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views  # views.py をインポート
 
+app_name = 'quiz'
+
 urlpatterns = [
     # views.IndexView クラスを .as_view() メソッドで呼び出す
     path('', views.IndexView.as_view(), name='index'),
+
+    # 'quiz/'というURLへのアクセスが来たら、views.QuizViewを呼び出す
+    path('quiz/', views.QuizView.as_view(), name='quiz_page'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Question
 
 
 class IndexView(TemplateView):
@@ -6,4 +7,22 @@ class IndexView(TemplateView):
     トップページを表示するクラスベースビュー
     """
     # 表示するテンプレート（HTML）ファイルを指定する
-    template_name = 'index.html'
+    template_name = 'quiz/index.html'
+
+
+class QuizView(ListView):
+    """
+    ランダムなクイズ問題を10問表示する
+    """
+    model = Question               # 1. どのモデルからデータを取得するか
+    template_name = 'quiz/quiz.html'    # 2. どのHTMLを使って表示するか
+    context_object_name = 'questions' # 3. HTML側に渡す変数名 (default: object_list)
+
+    def get_queryset(self):
+        """
+        クエリセットをカスタマイズして、ランダムな10件を取得する
+        """
+        # Question.objects.order_by('?') でランダムに並び替え、
+        # [:10] で最初の10件を取得する
+        queryset = super().get_queryset().order_by('?')[:10]
+        return queryset
